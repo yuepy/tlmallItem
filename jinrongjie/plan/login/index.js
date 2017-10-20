9a84aaccd52c236c38d14a844690a80380b6bc7f
lianshun.du <lianshun.du@yunshipei.com>
2017-10-20 11:02:46 +0800

    fixed

---
+++ jinrongjie/plan/login/index.js
@@ -0,0 +1,10 @@
+(function (win, ysp) {
+  ysp.runtime.Model.extendLoadingModel({
+    getData_control5_CNTcxJ: function (elem) {},
+    doAction_uiControl5_zOagGr: function (data, elem) {},
+    getTemplate_uiControl5_zOagGr: function () {
+      var selfTemplate = "module.exports = React.createClass({\n  render: function() {\n    return (\n      <div style={{\n          textAlign:'center',\n          padding:'8px',\n          background:'#333',\n          color:'#fff',\n          fontSize:'18'\n        }}>\n        \u767B \u5F55\n      </div>\n    )\n  }\n});";
+      return "'use strict';\n\nmodule.exports = React.createClass({\n  displayName: 'exports',\n\n  render: function render() {\n    return React.createElement(\n      'div',\n      { style: {\n          textAlign: 'center',\n          padding: '8px',\n          background: '#333',\n          color: '#fff',\n          fontSize: '18'\n        } },\n      '\\u767B \\u5F55'\n    );\n  }\n});";
+    }
+  });
+})(window, ysp);
