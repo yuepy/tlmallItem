c911ce2b18e2ba59961079c2c8c2cb18b6c7e4c1
lianshun.du <lianshun.du@yunshipei.com>
2017-11-15 16:36:54 +0800

    fixed

--- jinrongjie/plan/managerAuditRelatedDoc/index.js
+++ jinrongjie/plan/managerAuditRelatedDoc/index.js
@@ -5,9 +5,9 @@
     getData_control32_RBmFTr: function (elem) {},
     doAction_uiControl18_79vAeM: function (data, elem) {
       if (data.eventType == 'click') {
-        ysp.customHelper.back();
-      }if (data.eventType == 'save1') {
-        debugger;elem.ownerDocument.defaultView.btnok_onclick();ysp.customHelper.back();
+        window.EAPI.back();
+      };if (data.eventType == 'save1') {
+        elem.ownerDocument.defaultView.btnok_onclick();ysp.customHelper.back();
       }
     },
     getTemplate_uiControl18_79vAeM: function getTemplate_uiControl18_79vAeM() {
