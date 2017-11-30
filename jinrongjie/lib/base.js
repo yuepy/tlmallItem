72917235b1df6c68f6263d33cef5e2f5d619885c
lianshun.du <lianshun.du@yunshipei.com>
2017-11-30 19:17:09 +0800

    地址变量

--- jinrongjie/lib/base.js
+++ jinrongjie/lib/base.js
@@ -460,69 +460,71 @@
           }
 
           /* 退回流程 */
-          // aWin.doReject_New = function(){
-          //   debugger;
-          //   aWin.getRemarkText_log();
-          //   aWin.doReject();
-          // }
+          aWin.doReject_New = function(){
+            aWin.getRemarkText_log();
+            aWin.doReject();
+          }
           
-//           aWin.onSetRejectNode = function(){
-//             debugger;
-//             console.log(aWin.onSetRejectNode.toString())
-            
-//             var $G = aWin.$G;
-//          var url=aWin.escape("/workflow/request/RejectNodeSet.jsp?requestid=407439&workflowid=8061&nodeid=121109&isrejectremind=1&ischangrejectnode=1&isselectrejectnode=1&isFreeNode=");
-//             var result = aWin.showModalDialog("/systeminfo/BrowserMain.jsp?url="+url);
-//             aWin.setReturnValue = function (result){
-//                 if (result != null) {
-//                    // $G("RejectNodes").value=result;
-//                 var val=result.split("|");
-//                   if($G("RejectNodes")) $G("RejectNodes").value=val[0];
-//                   if($G("RejectToNodeid")) $G("RejectToNodeid").value=val[1]; 
-//                  return true;
-//               }else{
-//                   return false;
-//               }
-//             }
-//         }
+          if(aWin.onSetRejectNode){
+            aWin.onSetRejectNode = function(){
+            	var $G = aWin.$G;
+              var requestidId = doc.getElementById('requestid').value;
+              var workflowId = doc.getElementById('workflowid').value;
+              var nodeId = doc.querySelector('input[name="nodeid"]').value;
+             	var url=aWin.escape("/workflow/request/RejectNodeSet.jsp?requestid="+requestidId+"&workflowid="+workflowId+"&nodeid="+nodeId+"&isrejectremind=1&ischangrejectnode=1&isselectrejectnode=1&isFreeNode=");
+                var result = aWin.showModalDialog("/systeminfo/BrowserMain.jsp?url="+url);
+                aWin.setReturnValue = function (result){
+                    if (result != null) {
+                       // $G("RejectNodes").value=result;
+                    var val=result.split("|");
+                      if($G("RejectNodes")) $G("RejectNodes").value=val[0];
+                      if($G("RejectToNodeid")) $G("RejectToNodeid").value=val[1]; 
+                     return true;
+                  }else{
+                      return false;
+                  }
+                }
+            }
+          }
           
-//           aWin.onsave = function(){
-//             var nodeids="";
-//             var $G = aWin.$G;
-//             if($G("checkall").checked){ 
-//              nodeids="-1";
-//             }else{ 
+          
+          aWin.onsave = function(){
+            var nodeids="";
+            var $G = aWin.$G;
+            if($G("checkall").checked){ 
+             nodeids="-1";
+            }else{ 
 
-//                 if($G("nodeid_121108").checked){
-//                     if(nodeids.length>0){
-//                         nodeids+=","+$G("nodeid_121108").value;
-//                     }else{
-//                         nodeids=$G("nodeid_121108").value;
-//                     }
-//                 }
+                if($G("nodeid_121108").checked){
+                    if(nodeids.length>0){
+                        nodeids+=","+$G("nodeid_121108").value;
+                    }else{
+                        nodeids=$G("nodeid_121108").value;
+                    }
+                }
 
-//                 if($G("nodeid_121109").checked){
-//                     if(nodeids.length>0){
-//                         nodeids+=","+$G("nodeid_121109").value;
-//                     }else{
-//                         nodeids=$G("nodeid_121109").value;
-//                     }
-//                 }
+                if($G("nodeid_121109").checked){
+                    if(nodeids.length>0){
+                        nodeids+=","+$G("nodeid_121109").value;
+                    }else{
+                        nodeids=$G("nodeid_121109").value;
+                    }
+                }
 
-//                 //alert(nodeids);
-//                    }
-//             var rejectnodeid="";
-//             rejectnodeid=aWin.getRadioValue("rejectnodeid");
-//             if(rejectnodeid==""){
-//                 alert("请选择退回节点");
-//                 return false;
-//             }
-//             aWin.parent.opener.setReturnValue(nodeids+"|"+rejectnodeid);
-//             if(aWin.parent.opener.setReturnValue(nodeids+"|"+rejectnodeid)){
-//                aWin.parent.opener.showtipsinfo(380971,8061,121109,1,1,3151,"","reject","2","divFavContent18980","正在退回流程，请稍等....")
-//                }
-//             aWin.parent.close();
-//         }
+                //alert(nodeids);
+                   }
+            var rejectnodeid="";
+            rejectnodeid=aWin.getRadioValue("rejectnodeid");
+            if(rejectnodeid==""){
+                alert("请选择退回节点");
+                return false;
+            }
+            aWin.parent.opener.setReturnValue(nodeids+"|"+rejectnodeid);
+            if(aWin.parent.opener.setReturnValue(nodeids+"|"+rejectnodeid)){
+               aWin.parent.opener.showtipsinfo(380971,8061,121109,1,1,3151,"","reject","2","divFavContent18980","正在退回流程，请稍等....")
+               }
+            aWin.parent.close();
+        }
           
           
         },
@@ -538,6 +540,9 @@
             // }
 
             // 测试
+          if(aWin.onSetRejectNode){
+             console.log(aWin.onSetRejectNode.toString())
+             }
             var fakeFormId;
           	var uploadId;
             (function() {
