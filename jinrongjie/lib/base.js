636d109a0e8fc7d191e25c27ce0e7b7d3389186a
zhuyatong <yatong.zhu@yunshipei.com>
2017-12-04 15:44:01 +0800

    送阅

--- jinrongjie/lib/base.js
+++ jinrongjie/lib/base.js
@@ -397,7 +397,12 @@
             }
             aWin.doReview = function() {
                 // jQuery($GetEle("flowbody")).attr("onbeforeunload", "");
+              if(doc.getElementById('flowbody')){
                 doc.getElementById('flowbody').setAttribute('onbeforeunload', '')
+              }else if(doc.getElementById('bodyiframe')){
+                doc.getElementById('bodyiframe').setAttribute('onbeforeunload', '')
+              }
+                
                 aWin.doLocationHref();
             }
             aWin.checkfileuploadcomplet = function() {
