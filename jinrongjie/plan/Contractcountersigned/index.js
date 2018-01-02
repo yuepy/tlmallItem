adbcf92189617889cfdddd14e55b818214ead37e
zhuyatong <yatong.zhu@yunshipei.com>
2018-01-02 17:33:43 +0800

    返回

--- jinrongjie/plan/Contractcountersigned/index.js
+++ jinrongjie/plan/Contractcountersigned/index.js
@@ -90,15 +90,16 @@
       if (data.eventType == 'click') {
         //ysp.customHelper.back(); 
         //history.go(-1); // ysp.customHelper.forceMatchModels("waitTodo"); //ysp.appMain.back();
-        var _url = elem.ownerDocument.defaultView.location.href;if (_url.indexOf("viewType=0") != -1) {
-          ysp.appMain.reloadPage("http://192.168.200.63/workflow/request/RequestView.jsp?needPopupNewPage=true");
-        } else if (_url.indexOf("viewType=2") != -1) {
-          ysp.appMain.reloadPage("http://192.168.200.63/workflow/request/RequestHandled.jsp?needPopupNewPage=true");
-        } else if (_url.indexOf("viewType=3") != -1) {
-          ysp.appMain.reloadPage("http://192.168.200.63/workflow/request/RequestComplete.jsp?needPopupNewPage=true");
-        } else if (_url.indexOf("viewType=4") != -1) {
-          ysp.appMain.reloadPage("http://192.168.200.63/workflow/request/MyRequestView.jsp?needPopupNewPage=true");
-        }
+        var _url = elem.ownerDocument.defaultView.location.href; // if (_url.indexOf("viewType=0") != -1) {
+        //   ysp.appMain.reloadPage("http://192.168.200.63/workflow/request/RequestView.jsp?needPopupNewPage=true");
+        // } else if (_url.indexOf("viewType=2") != -1) {
+        //   ysp.appMain.reloadPage("http://192.168.200.63/workflow/request/RequestHandled.jsp?needPopupNewPage=true");
+        // } else if (_url.indexOf("viewType=3") != -1) {
+        //   ysp.appMain.reloadPage("http://192.168.200.63/workflow/request/RequestComplete.jsp?needPopupNewPage=true");
+        // } else if (_url.indexOf("viewType=4") != -1) {
+        //   ysp.appMain.reloadPage("http://192.168.200.63/workflow/request/MyRequestView.jsp?needPopupNewPage=true");
+        // }
+        ysp.customHelper.back();
       }if (_url.match(/WFSearchResult\.jsp$/)) {
         elem.ownerDocument.defaultView.location.href = elem.ownerDocument.defaultView.localStorage.testURL;
       }
@@ -133,6 +134,18 @@
     getTemplate_uiControl2_4A5vOm: function getTemplate_uiControl2_4A5vOm() {
       var selfTemplate = 'import {Component} from \'react\';\nexport default class extends Component{\n  render(){\n    return(\n    \t<div className=\'ysp-pageTurnoperation\'>{\n          !this.props.customData.buttonPrev ?\n          <button onClick={(e)=>{\n            YSP.appRenderer.showLoading();\n            var handler = this.props.customHandler;\n            if(handler){\n              handler({\n                eventType:\'prev\'\n              })\n            }\n          }}>\u524D\u4E00\u9875</button> : <button>\u524D\u4E00\u9875</button> \n        }\n        \n        <span>{this.props.customData.data ?this.props.customData.data:\'0\' }</span>\n        {!this.props.customData.buttonNext ?\n          <button onClick={(e)=>{\n            YSP.appRenderer.showLoading();\n            var handler = this.props.customHandler;\n            if(handler){\n              handler({\n                eventType:\'next\'\n              })\n            }\n          }}>\u540E\u4E00\u9875</button> : <button>\u540E\u4E00\u9875</button>\n        }\n      </div>\n    )\n  }\n}';
       return '\'use strict\';\n\nObject.defineProperty(exports, "__esModule", {\n  value: true\n});\n\nvar _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();\n\nvar _react = require(\'react\');\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }\n\nfunction _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn\'t been initialised - super() hasn\'t been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }\n\nfunction _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }\n\nvar _class = function (_Component) {\n  _inherits(_class, _Component);\n\n  function _class() {\n    _classCallCheck(this, _class);\n\n    return _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).apply(this, arguments));\n  }\n\n  _createClass(_class, [{\n    key: \'render\',\n    value: function render() {\n      var _this2 = this;\n\n      return React.createElement(\n        \'div\',\n        { className: \'ysp-pageTurnoperation\' },\n        !this.props.customData.buttonPrev ? React.createElement(\n          \'button\',\n          { onClick: function onClick(e) {\n              YSP.appRenderer.showLoading();\n              var handler = _this2.props.customHandler;\n              if (handler) {\n                handler({\n                  eventType: \'prev\'\n                });\n              }\n            } },\n          \'\\u524D\\u4E00\\u9875\'\n        ) : React.createElement(\n          \'button\',\n          null,\n          \'\\u524D\\u4E00\\u9875\'\n        ),\n        React.createElement(\n          \'span\',\n          null,\n          this.props.customData.data ? this.props.customData.data : \'0\'\n        ),\n        !this.props.customData.buttonNext ? React.createElement(\n          \'button\',\n          { onClick: function onClick(e) {\n              YSP.appRenderer.showLoading();\n              var handler = _this2.props.customHandler;\n              if (handler) {\n                handler({\n                  eventType: \'next\'\n                });\n              }\n            } },\n          \'\\u540E\\u4E00\\u9875\'\n        ) : React.createElement(\n          \'button\',\n          null,\n          \'\\u540E\\u4E00\\u9875\'\n        )\n      );\n    }\n  }]);\n\n  return _class;\n}(_react.Component);\n\nexports.default = _class;';
+    },
+    getData_control351_Tg62Gm: function (elem) {
+      return;
+    },
+    doAction_uiControl249_TSgyIu: function (data, elem) {
+      if (data.eventType == "click") {
+        debugger;alert("返回");console.log("返回");ysp.appMain.closeWindow();
+      }
+    },
+    getTemplate_uiControl249_TSgyIu: function () {
+      var selfTemplate = "module.exports = React.createClass({\n  click:function(e){\n    var handler=this.props.customHandler;\n    if(handler){\n      handler({\n        eventType:\"click\"\n      })\n    }\n  },\n  render: function() {\n    return (\n      <div onClick={this.click.bind(this)}>\n       \n      </div>\n    )\n  }\n});";
+      return "\"use strict\";\n\nmodule.exports = React.createClass({\n  displayName: \"exports\",\n\n  click: function click(e) {\n    var handler = this.props.customHandler;\n    if (handler) {\n      handler({\n        eventType: \"click\"\n      });\n    }\n  },\n  render: function render() {\n    return React.createElement(\"div\", { onClick: this.click.bind(this) });\n  }\n});";
     }
   });
 })(window, ysp);
