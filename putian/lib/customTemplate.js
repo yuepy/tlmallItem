2382874dec2633073fbe23b465798ab58d480de9
735539908 <yanghaijun147@sina.com>
2018-05-03 11:08:02 +0800

    删除无用项目

--- putian/lib/customTemplate.js
+++
@@ -1,9 +0,0 @@
-(function (win, ysp) {
-
-  var utils = ysp.utils;
-  ysp.customTemplateHelper = {};
-  utils.extend(ysp.customTemplateHelper, {
-
-  });
-
-})(window, ysp);
--- putianBPM/override/top.js
+++
@@ -1,7 +0,0 @@
-	 if(window.getTop()!=window){
-		window.getTop().location = window.location;
-	 }
-	 
-	 function getTop(){
-	    return _ysp_top;
-	 }
