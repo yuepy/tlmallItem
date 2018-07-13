c9c92822b81dcb685b4a2e25a3048b3584246363
梁宇豪 <yuhao.liang@yunshipei.com>
2018-07-13 14:02:04 +0800

    将老框架代码合并至当前分支.准备合并两套环境代码 . 当前时间代码为两套环境最新代码

---
+++ putian_crm_v2/plan/achievementSecondMenu/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function (win, ysp) {
+  ysp.runtime.Model.extendLoadingModel({
+    getData_control117_K3I153: function getData_control117_K3I153(elem) {
+      if (!elem) {
+        return;
+      }return ysp.customHelper.statusManager && ysp.customHelper.statusManager.currentStatus;
+    },
+    doAction_uiControl108_dMnVnu: function doAction_uiControl108_dMnVnu(data, elem) {},
+    getTemplate_uiControl108_dMnVnu: function getTemplate_uiControl108_dMnVnu() {
+      var selfTemplate = "import {Component} from 'react';\nimport {StatusTip} from 'ysp-custom-components';\nexport default class extends Component{\n  constructor(props){\n    super(props);\n  }\n  render(){\n    const status = this.props.customData;\n    return (\n      <StatusTip\n        status = {status}\n        />\n    )\n  }\n}";
+      return "'use strict';\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nvar _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();\n\nvar _react = require('react');\n\nvar _yspCustomComponents = require('ysp-custom-components');\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError(\"this hasn't been initialised - super() hasn't been called\"); } return call && (typeof call === \"object\" || typeof call === \"function\") ? call : self; }\n\nfunction _inherits(subClass, superClass) { if (typeof superClass !== \"function\" && superClass !== null) { throw new TypeError(\"Super expression must either be null or a function, not \" + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }\n\nvar _class = function (_Component) {\n  _inherits(_class, _Component);\n\n  function _class(props) {\n    _classCallCheck(this, _class);\n\n    return _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).call(this, props));\n  }\n\n  _createClass(_class, [{\n    key: 'render',\n    value: function render() {\n      var status = this.props.customData;\n      return React.createElement(_yspCustomComponents.StatusTip, {\n        status: status\n      });\n    }\n  }]);\n\n  return _class;\n}(_react.Component);\n\nexports.default = _class;";
+    }
+  }, "achievementSecondMenu");
+})(window, ysp);
