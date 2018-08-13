9c5d376a53793b44a99f496d189aaea61700963b
jiangboxu <jiangbo.xu@yunshipei.com>
2018-08-13 20:19:45 +0800

    0813质检总局项目login及index开发

--- ZhiJianZongJu/plan/index_itownet/index.js
+++ ZhiJianZongJu/plan/index_itownet/index.js
@@ -1,5 +1,34 @@
-(function(win, ysp) {
+(function (win, ysp) {
   ysp.runtime.Model.extendLoadingModel({
-
-  });
+    getData_control3_017ukM: function (elem) {},
+    doAction_uiControl3_ARdezt: function (data, elem) {
+      if (data.eventType == 'click') {
+        debugger;elem.ownerDocument.defaultView.open('http://egs.itownet.cn:9080/InspMeterMobile//artificial/taskAssignmentView.jsp?tokenInfo=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyQ29kZSI6InJvb3QiLCJpc3MiOiJhdXRoMCIsInBvcnRsZXRJZCI6Im5ld0FwcC0xMiJ9.Q-xFW_03TSan6pGJiNI46SgHbNs5S0TfRrXl9mOEg0MFR54Vx9ZhW35D4XefqF8jHYompi3bXVwzTQHHl--B5mESri6X0j7bj7HbfBzDyFdgptHtgW98F9WNtd5jnktxYseB6XhnW8X2Z2JhH0vEKPFu5Z59B7oZxoi2YX2wdaw');
+      }
+    },
+    getTemplate_uiControl3_ARdezt: function () {
+      var selfTemplate = 'module.exports = React.createClass({\n  onClick:function(e){\n    var handler = this.props.customHandler,\n        target = e.target;\n    if(handler){\n      handler({\n        eventType:\'click\'\n      })\n    }\n  },\n  render: function() {\n    return (\n      <div className=\'itwont\'>\n        <button onClick={this.onClick.bind(this)}>\u4EBA\u5DE5\u5206\u914D</button>\n      </div>\n    )\n  }\n});';
+      return '\'use strict\';\n\nmodule.exports = React.createClass({\n  displayName: \'exports\',\n\n  onClick: function onClick(e) {\n    var handler = this.props.customHandler,\n        target = e.target;\n    if (handler) {\n      handler({\n        eventType: \'click\'\n      });\n    }\n  },\n  render: function render() {\n    return React.createElement(\n      \'div\',\n      { className: \'itwont\' },\n      React.createElement(\n        \'button\',\n        { onClick: this.onClick.bind(this) },\n        \'\\u4EBA\\u5DE5\\u5206\\u914D\'\n      )\n    );\n  }\n});';
+    },
+    getData_control4_Z0FrGU: function (elem) {},
+    doAction_uiControl5_gs2qFW: function (data, elem) {
+      if (data.eventType == 'click') {
+        elem.ownerDocument.defaultView.open('http://egs.itownet.cn:9080/dataApEsp/homepage/homepage.jsp?tokenInfo=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyQ29kZSI6InJvb3QiLCJpc3MiOiJhdXRoMCIsInBvcnRsZXRJZCI6Im5ld0FwcDEzIn0.OlDYfwa-4T8hzsplTfW03ibgO6U26Vt-VkP2kZmoc5lHhIAF-je2qtpVKUEFDK8uYLqoyrvcu64zbbFghHALUHIrJIfLQ03V_zwsX2B14f8e-uxepnRDZRwtCk6lx4CNg22r_rVRgyIbsoO2NxpH3E0qNO41xAb4hErqAIzbnnw');
+      }
+    },
+    getTemplate_uiControl5_gs2qFW: function () {
+      var selfTemplate = 'module.exports = React.createClass({\n  onClick:function(e){\n    var handler = this.props.customHandler,\n        target = e.target;\n    if(handler){\n      handler({\n        eventType:\'click\'\n      })\n    }\n  },\n  render: function() {\n    return (\n      <div className=\'itwont\'>\n        <button onClick={this.onClick.bind(this)}>\u6570\u5B57\u8D28\u91CF\u76D1\u7763</button>\n      </div>\n    )\n  }\n});';
+      return '\'use strict\';\n\nmodule.exports = React.createClass({\n  displayName: \'exports\',\n\n  onClick: function onClick(e) {\n    var handler = this.props.customHandler,\n        target = e.target;\n    if (handler) {\n      handler({\n        eventType: \'click\'\n      });\n    }\n  },\n  render: function render() {\n    return React.createElement(\n      \'div\',\n      { className: \'itwont\' },\n      React.createElement(\n        \'button\',\n        { onClick: this.onClick.bind(this) },\n        \'\\u6570\\u5B57\\u8D28\\u91CF\\u76D1\\u7763\'\n      )\n    );\n  }\n});';
+    },
+    getData_control7_3qAPSe: function (elem) {},
+    doAction_uiControl6_RrcIOM: function (data, elem) {
+      if (data.eventType == 'click') {
+        elem.ownerDocument.defaultView.open('http://egs.itownet.cn:9080/mobileProject/fieldSampling/list.jsp?tokenInfo=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyQ29kZSI6InJvb3QiLCJpc3MiOiJhdXRoMCIsInBvcnRsZXRJZCI6IjEwMTAxMTExMDAifQ.YbnCoVt6qhjBgA7Z3XqevbDzHCfGmfa4wAl7kDd_pd3m2GZqFQSAmuol0f7fx3RX5BuPeJQGYy96oNT_GbXKOZwWaaZkFwPmcgIbeJgFayY1osgpOP8J-LtbdVxBFuiCtw_zvRHnzK-XHF8dSy6AYyDdZzHtwz7kYU0kDKCyNRs');
+      }
+    },
+    getTemplate_uiControl6_RrcIOM: function () {
+      var selfTemplate = 'module.exports = React.createClass({\n  onClick:function(e){\n    var handler = this.props.customHandler,\n        target = e.target;\n    if(handler){\n      handler({\n        eventType:\'click\'\n      })\n    }\n  },\n  render: function() {\n    return (\n      <div className=\'itwont\'>\n        <button onClick={this.onClick.bind(this)}>\u73B0\u573A\u62BD\u6837</button>\n      </div>\n    )\n  }\n});';
+      return '\'use strict\';\n\nmodule.exports = React.createClass({\n  displayName: \'exports\',\n\n  onClick: function onClick(e) {\n    var handler = this.props.customHandler,\n        target = e.target;\n    if (handler) {\n      handler({\n        eventType: \'click\'\n      });\n    }\n  },\n  render: function render() {\n    return React.createElement(\n      \'div\',\n      { className: \'itwont\' },\n      React.createElement(\n        \'button\',\n        { onClick: this.onClick.bind(this) },\n        \'\\u73B0\\u573A\\u62BD\\u6837\'\n      )\n    );\n  }\n});';
+    }
+  }, "index_itownet");
 })(window, ysp);
