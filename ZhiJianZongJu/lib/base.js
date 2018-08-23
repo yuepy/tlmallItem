bc5885beee6a6704ec5e6a0d18bab5119d62c3e8
unknown <>
2018-08-23 14:39:04 +0800

    add addSamlping page

--- ZhiJianZongJu/lib/base.js
+++ ZhiJianZongJu/lib/base.js
@@ -199,8 +199,42 @@
     //思路与needToLogin类似, 保证能够唯一区分该页面即可.
     isLoginSuccess: function(doc) {
       return false;
+    },
+     fireKeyEvent: function (el, evtType, keyCode) {
+        var doc = el.ownerDocument,
+            win = doc.defaultView || doc.parentWindow,
+            evtObj;
+        if (doc.createEvent) {
+            if (win.KeyEvent) {
+                evtObj = doc.createEvent('KeyEvents');
+                evtObj.initKeyEvent(evtType, true, true, win, false, false, false, false, keyCode, 0);
+            } else {
+                evtObj = doc.createEvent('UIEvents');
+                Object.defineProperty(evtObj, 'keyCode', {
+                    get: function () {
+                        return this.keyCodeVal;
+                    }
+                });
+                Object.defineProperty(evtObj, 'which', {
+                    get: function () {
+                        return this.keyCodeVal;
+                    }
+                });
+                evtObj.initUIEvent(evtType, true, true, win, 1);
+                evtObj.keyCodeVal = keyCode;
+                if (evtObj.keyCode !== keyCode) {
+                    console.log("keyCode " + evtObj.keyCode + " 和 (" + evtObj.which + ") 不匹配");
+                }
+            }
+            el.dispatchEvent(evtObj);
+        } else if (doc.createEventObject) {
+            evtObj = doc.createEventObject();
+            evtObj.keyCode = keyCode;
+            el.fireEvent('on' + evtType, evtObj);
+        }
+        return false;
     }
-
+		
 
   });
 
