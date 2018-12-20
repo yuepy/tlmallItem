436a577f496ebb5ac0f796085991b2bb29570003
zhuyatong <yatong.zhu@yunshipei.com>
2018-12-20 16:25:58 +0800

    HR手势

--- putianIHR/plan/addWork/index.js
+++ putianIHR/plan/addWork/index.js
@@ -2,11 +2,11 @@
   ysp.runtime.Model.extendLoadingModel({
     getData_control49_6Hie6z: function (elem) {
       if (elem) {
-        var data = { vall: [], pserror: [] };var trs = elem.querySelectorAll("tr[id]");[].forEach.call(trs, function (d, i) {
+        var data = { vall: [], pserror: [] };var trs = elem.querySelectorAll("tr[id]");var href = elem.ownerDocument.defaultView.location.href;[].forEach.call(trs, function (d, i) {
           var arr = [];var errers = [];var inputs = d.querySelectorAll("input");[].forEach.call(inputs, function (d1, i1) {
             if (i1 == 0) {
               // arr.push(d1.value.replace(/\//g, "-"));
-              if (ysp.appMain.isIOS() && d1.getAttribute("id").indexOf("DATE") !== -1) {
+              if (ysp.appMain.isIOS() && d1.getAttribute("id").indexOf("DATE") !== -1 && href.indexOf("192.168.220.110") !== -1) {
                 var date = d1.value.split("/");arr.push(date[2] + "-" + date[1] + "-" + date[0]);
               } else {
                 arr.push(d1.value.replace(/\//g, "-"));
@@ -28,7 +28,7 @@
           var href = elem.ownerDocument.defaultView.location.href;if (ysp.appMain.isIOS() && href.indexOf("192.168.220.110") !== -1) {
             var date = group[1].split("-");trs[group[2]].querySelectorAll("input")[0].value = date[2] + "/" + date[1] + "/" + date[0];
           } else {
-            time.value = val.replace(/-/g, "/");
+            trs[group[2]].querySelectorAll("input")[0].value = group[1].replace(/-/g, "/");
           }trs[group[2]].querySelectorAll("input")[0].dispatchEvent(new Event("change"));trs[group[2]].querySelectorAll("input")[0].nextElementSibling.click();elem.ownerDocument.querySelector("#bodyCalendar").querySelector('a[tabindex="0"]').parentElement.click();
         } else if (group[0].indexOf("hours") != -1) {
           trs[group[2]].querySelectorAll("input")[1].value = group[1].replace(/-/g, "/");trs[group[2]].querySelectorAll("input")[1].dispatchEvent(new Event("change"));
