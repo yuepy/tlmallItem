fd2586b6d19c40142f2d9037b57436b7bce03cd5
徐江博 <18793473245@163.com>
2019-01-10 17:32:05 +0800

    1月10日智能生活馆产品系列趋势修改

--- CRM_NewPlatForm/override/chart.js
+++ CRM_NewPlatForm/override/chart.js
@@ -510,6 +510,32 @@ function getBarLine(datas,Id){
 			axisTick: { show: false //不显示刻度线
 			}
 			},
+    	    dataZoom : [
+	    	{
+                type: 'slider',
+                show: true,
+                start: 94,
+                end: 100,
+                handleSize: 8
+            },
+            {
+                type: 'inside',
+                start: 94,
+                end: 100
+            },
+            {
+                type: 'slider',
+                show: true,
+                yAxisIndex: 0,
+                filterMode: 'empty',
+                width: 12,
+                height: '70%',
+                handleSize: 8,
+                showDataShadow: false,
+                left: '93%'
+            }
+	    ],
+
 		series: [{ //柱状图bar设置
 			name: legendDatas[0],
 			type: "bar",
