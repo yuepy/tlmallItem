5a1b249bd70e2fe1c613ff8f086b935ebd7950eb
梁宇豪 <yuhao.liang@yunshipei.com>
2019-07-04 09:47:59 +0800

    fixed

--- CRM_NewPlatForm/lib/base.js
+++ CRM_NewPlatForm/lib/base.js
@@ -1,6 +1,18 @@
 'use strict';
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _defineProperty(obj, key, value) { 
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value, 
+      enumerable: true, 
+      configurable: true, 
+      writable: true 
+    }); 
+  } else { 
+    obj[key] = value; 
+  } 
+  return obj; 
+}
 
 //订阅模式
 (function (exports) {
